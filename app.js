f670a4998763a1c26f0a947091ee5b34228b2ad0
Miikasaaa11 <160037838+Miikasaaa11@users.noreply.github.com>
2024-02-22 11:50:30 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let userInput =document.querySelector('.userEntry');
+let finalCalc =
+document.querySelector('.finalCalc');
+let buttons = document.querySelectorAll('button');
+let operator = '';
+let operand1 = '';
+let operand2 = '';
+let result = 0;
+let isOperatorClicked = false;
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        let buttonValue = button.textContent;
+    }
+    )}
+)
